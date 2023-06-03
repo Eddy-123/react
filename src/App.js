8d3364e78da380b4671642926abbcad3f478ad15
Eddy-123 <adegnandjoueddy12@gmail.com>
2023-06-03 10:35:13 +0100

    maintainn temporary state for an employee

--- src/App.js
+++ src/App.js
@@ -1,56 +1,89 @@
-import './index.css';
-import Employee from './components/Employee';
-import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import "./index.css";
+import Employee from "./components/Employee";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const [role, setRole] = useState('dev');
+  const [role, setRole] = useState("dev");
   const [employees, setEmployees] = useState([
     {
+      id: 1,
       name: "Eddy",
       role: "Developper",
-      img: "https://picsum.photos/200/300"
-    },{
+      img: "https://picsum.photos/200/300",
+    },
+    {
+      id: 2,
       name: "Abby",
       role: "Developper",
-      img: "https://picsum.photos/200/300"
-    },{
+      img: "https://picsum.photos/200/300",
+    },
+    {
+      id: 3,
       name: "Adam",
       role: "Developper",
-      img: "https://picsum.photos/200/300"
-    },{
+      img: "https://picsum.photos/200/300",
+    },
+    {
+      id: 4,
       name: "Caleb",
       role: "Developper",
-      img: "https://picsum.photos/200/300"
-    },{
+      img: "https://picsum.photos/200/300",
+    },
+    {
+      id: 5,
       name: "Olivia",
       role: "Developper",
-      img: "https://picsum.photos/200/300"
-    },{
+      img: "https://picsum.photos/200/300",
+    },
+    {
+      id: 6,
       name: "Eden",
       role: "Developper",
-      img: "https://picsum.photos/200/300"
+      img: "https://picsum.photos/200/300",
     },
   ]);
+
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployees = employees.map((employee) => {
+      if (id == employee.id) {
+        employee.name = newName;
+        employee.role = newRole;
+      }
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  }
+
   const showEmployees = true;
   return (
     <div className="App">
-    { showEmployees ? (
-      <>
-        <input type="text" onChange={(e) => {
-          setRole(e.target.value);
-        }} />
-        <div className='flex flex-wrap justify-center'>
-          {employees.map((employee) => {
-            return (
-              <Employee key={uuidv4()} name={employee.name} role={employee.role} img={employee.img} />
-            );
-          })}
-        </div>
-      </>
+      {showEmployees ? (
+        <>
+          <input
+            type="text"
+            onChange={(e) => {
+              setRole(e.target.value);
+            }}
+          />
+          <div className="flex flex-wrap justify-center">
+            {employees.map((employee) => {
+              return (
+                <Employee
+                  key={employee.id}
+                  id={employee.id}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                  updateEmployee={updateEmployee}
+                />
+              );
+            })}
+          </div>
+        </>
       ) : (
         <p>You cannot see employees</p>
-      ) }
+      )}
     </div>
   );
 }
