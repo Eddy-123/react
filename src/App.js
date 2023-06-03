23df2099510a3bda0b65fdd9d17d240799e661d1
Eddy-123 <adegnandjoueddy12@gmail.com>
2023-06-03 12:14:17 +0100

    add new employee

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import AddEmployee from "./components/AddEmployee";
 
 function App() {
   const [role, setRole] = useState("dev");
@@ -55,6 +56,16 @@ function App() {
     setEmployees(updatedEmployees);
   }
 
+  function addEmployee(name, role, img) {
+    const newEmployee = {
+      id: uuidv4(),
+      name: name,
+      role: role,
+      img: img,
+    };
+    setEmployees([...employees, newEmployee]);
+  }
+
   const showEmployees = true;
   return (
     <div className="App">
@@ -80,6 +91,7 @@ function App() {
               );
             })}
           </div>
+          <AddEmployee addEmployee={addEmployee} />
         </>
       ) : (
         <p>You cannot see employees</p>
